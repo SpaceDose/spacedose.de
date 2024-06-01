8b4f2db333efc2ea7f28abb819bcb6bcd4636ca0
SpaceDose <jan.ellermann@spacedose.de>
2024-06-01 21:59:57 +0200

    add verson nomba

--- web/src/home.tsx
+++ web/src/home.tsx
@@ -3,9 +3,13 @@ import {Link} from 'react-router-dom';
 import {Page} from './page';
 
 export const Home: FC = () => (
-  <Page title='Home' className='flex items-center justify-center'>
+  <Page
+    title='Home'
+    className='flex flex-col items-center justify-center gap-2'
+  >
     <Link to='/kcal' className='rounded-lg bg-orange-600 p-2 text-white'>
       ⊂(◉‿◉)つ
     </Link>
+    <p className='text-sm font-thin'>0.0.0</p>
   </Page>
 );
