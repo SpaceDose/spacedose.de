eb4996707e8a22e370b179078ec242262cd0e249
SpaceDose <jan.ellermann@spacedose.de>
2024-06-01 20:49:20 +0200

    more

--- web/src/home.tsx
+++ web/src/home.tsx
@@ -1,8 +1,11 @@
 import {type FC} from 'react';
+import {Link} from 'react-router-dom';
 import {Page} from './page';
 
 export const Home: FC = () => (
   <Page title='Home' className='flex items-center justify-center'>
-    Home
+    <Link to='/kcal' className='rounded-lg bg-orange-600 p-2 text-white'>
+      ⊂(◉‿◉)つ
+    </Link>
   </Page>
 );
